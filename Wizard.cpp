04b69c0bcacd1240b83aa228308290cc1a2b4a1b
Marcin Kłak <klak.marcin01@gmail.com>
2023-04-29 13:03:36 +0200

    Komentarz w main()

--- Wizard.cpp
+++ Wizard.cpp
@@ -27,7 +27,7 @@ int main()
     Wojownik w1("Czesio", 830);
     w1.wyswietl(); // Wojownik dziedziczy po Magu, wiec ma dostep do jego metod
     Statystyki s3(w1,true,600,60,70); 
-    s3.wyswietl();
+    s3.wyswietl(); // Statystyki wojownika
 
     return 0;
 }
